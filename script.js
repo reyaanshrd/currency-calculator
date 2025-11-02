da533e10b5dce8a5f3518f643a5cd4fe800d0351
reyaanshrd <milkhumama@gmail.com>
2025-11-02 11:08:13 +0530

    d

--- script.js
+++ script.js
@@ -11,4 +11,40 @@ function convert() {
         output_currency = input_currency*0.27
         document.getElementById("twoinput").innerHTML=output_currency
     }
+    if (currency_from=="AED" && currency_to=="EUR") {
+        output_currency = input_currency*0.24
+        document.getElementById("twoinput").innerHTML=output_currency
+    }
+    if (currency_from=="AED" && currency_to=="JPY") {
+        output_currency = input_currency*41.98
+        document.getElementById("twoinput").innerHTML=output_currency
+    }
+    if (currency_from=="AED" && currency_to=="GBP") {
+        output_currency = input_currency*0.21
+        document.getElementById("twoinput").innerHTML=output_currency
+    }
+    if (currency_from=="AED" && currency_to=="CAD") {
+        output_currency = input_currency*0.38
+        document.getElementById("twoinput").innerHTML=output_currency
+    }
+    if (currency_from=="AED" && currency_to=="CNY") {
+        output_currency = input_currency*1.94
+        document.getElementById("twoinput").innerHTML=output_currency
+    }
+    if (currency_from=="AED" && currency_to=="INR") {
+        output_currency = input_currency*24.17
+        document.getElementById("twoinput").innerHTML=output_currency
+    }
+    if (currency_from=="AED" && currency_to=="CHF") {
+        output_currency = input_currency*0.22
+        document.getElementById("twoinput").innerHTML=output_currency
+    }
+    if (currency_from=="AED" && currency_to=="BRL") {
+        output_currency = input_currency*1.46
+        document.getElementById("twoinput").innerHTML=output_currency
+    }
+    if (currency_from=="AED" && currency_to=="ZAR") {
+        output_currency = input_currency*4.74
+        document.getElementById("twoinput").innerHTML=output_currency
+    }
 }
